5ed6661786c7c1390d7226e368b87e8bc2aeb7e3
Raven <karascanvas@qq.com>
2020-05-31 23:49:09 +0800

    每个协程使用不同的变量递增，代替使用 atomic 对全局变量递增

--- main.go
+++ main.go
@@ -5,14 +5,75 @@ import (
 	"os"
 	"path"
 	"sync"
-	"sync/atomic"
 	"time"
 )
 
 type Context struct {
-	Errors    int32
-	Files     int32
-	EmptyDirs int32
+	Errors    int
+	Files     int
+	EmptyDirs int
+}
+
+func main() {
+	start := time.Now()
+	log.SetOutput(os.Stdout)
+
+	workers := 4
+	rootDir := "/"
+
+	var wg sync.WaitGroup
+	dirCh := make(chan string, 19999999)
+	wg.Add(1)
+	dirCh <- rootDir
+	ctxs := []*Context{}
+
+	for i := 0; i < workers; i++ {
+		ctx := new(Context)
+		ctxs = append(ctxs, ctx)
+		go func(c *Context) {
+			for {
+				dir, ok := <-dirCh
+				if ! ok {
+					return
+				}
+				infos, err := readDir(dir)
+				if err != nil {
+					c.Errors++
+				} else if len(infos) < 1 {
+					c.EmptyDirs++
+				} else {
+					for _, info := range infos {
+						if info.IsDir() {
+							wg.Add(1)
+							dirCh <- path.Join(dir, info.Name())
+						} else {
+							c.Files++
+						}
+					}
+				}
+				wg.Done()
+			}
+		}(ctx)
+	}
+
+	wg.Wait()
+	close(dirCh)
+
+	errors := 0
+	files := 0
+	emptyDirs := 0
+	for _, ctx := range ctxs {
+		errors += ctx.Errors
+		files += ctx.Files
+		emptyDirs += ctx.EmptyDirs
+	}
+
+	elapsed := time.Since(start)
+
+	log.Printf("Elapsed: %s\n", elapsed)
+	log.Printf("Errors: %d\n", errors)
+	log.Printf("Files: %d\n", files)
+	log.Printf("Empty Dirs: %d\n", emptyDirs)
 }
 
 func readDir(dirname string) ([]os.FileInfo, error) {
@@ -26,54 +87,4 @@ func readDir(dirname string) ([]os.FileInfo, error) {
 		return nil, err
 	}
 	return list, nil
-}
-
-func main() {
-	start := time.Now()
-	log.SetOutput(os.Stdout)
-
-	workers := 4
-	rootDir := "/"
-
-	ctx := new(Context)
-	var wg sync.WaitGroup
-	dirCh := make(chan string, 99999999)
-	wg.Add(1)
-	dirCh <- rootDir
-
-	for i := 0; i < workers; i++ {
-		go func() {
-			for {
-				dir, ok := <-dirCh
-				if ! ok {
-					return
-				}
-				infos, err := readDir(dir)
-				if err != nil {
-					atomic.AddInt32(&ctx.Errors, 1)
-				} else if len(infos) < 1 {
-					atomic.AddInt32(&ctx.EmptyDirs, 1)
-				} else {
-					for _, info := range infos {
-						if info.IsDir() {
-							wg.Add(1)
-							dirCh <- path.Join(dir, info.Name())
-						} else {
-							atomic.AddInt32(&ctx.Files, 1)
-						}
-					}
-				}
-				wg.Done()
-			}
-		}()
-	}
-
-	wg.Wait()
-	close(dirCh)
-	elapsed := time.Since(start)
-
-	log.Printf("Elapsed: %s\n", elapsed)
-	log.Printf("Errors: %d\n", ctx.Errors)
-	log.Printf("Files: %d\n", ctx.Files)
-	log.Printf("Empty Dirs: %d\n", ctx.EmptyDirs)
 }
